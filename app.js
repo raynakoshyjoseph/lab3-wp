81590bb9fe9d58248f2576a1aef904dfb65dcdfd
raynakoshyjoseph <90832641+raynakoshyjoseph@users.noreply.github.com>
2021-11-16 10:07:22 +0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+//creating app
+const app = express();
+//make the app listen on port
+const port = process.argv[2] || process.env.PORT || 3000;
+const server = app.listen(port, () => {
+  console.log(`Cart app listening at http://localhost:${port}`);
+});
+//send the index.html when receiving HTTP GET /
+app.use(express.static("public"));
+app.get("/", (req, res) => {
+  res.sendFile("index.html", { root: __dirname });
+});
+//handling static HTML and EJS templates
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.get("/", (req, res) => {
+  res.render("index"); //no need for ejs extension
+});
+//route for contacts
+app.get("/contacts", (req, res) => {
+  res.render("contacts");
+});
